4648a9e395513e5dfd7d6376433ed717ca51b15b
juanftech <juan.restrepo@techandsolve.com>
2018-09-10 19:51:51 -0500

    Se crea implementacion para navegar entre la MainActivity y Mas opciones para escoger un estilo de puerta al cual hacerle despiece

---
+++ app/src/main/java/co/com/uco/carmeloadmin/MasOpcionesDisenosActivity.java
@@ -0,0 +1,13 @@
+package co.com.uco.carmeloadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MasOpcionesDisenosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mas_opciones_disenos);
+    }
+}
